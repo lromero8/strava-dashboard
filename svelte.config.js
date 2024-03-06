e439fdc91e6e2ff0a1dc223531fc684296b5d17d
Luis Romero <luis.romero@atlascopco.com>
2024-03-06 14:59:23 +0100

    chore(netlify): Remove deploy yml

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 // import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-netlify';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+// import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
