3bc92872b220dfd82629f43cc6c84fabac62d239
Luis Romero <luis.romero@atlascopco.com>
2024-03-06 09:42:30 +0100

    feat(client): Get activities.

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,32 @@
+import { STRAVA_KEY } from '$env/static/private';
+
+interface Stats {
+    recent_run_totals: string,
+    all_run_totals: string,
+    recent_swim_totals: string,
+    biggest_ride_distance: number,
+    ytd_swim_totals: string,
+    all_swim_totals: string,
+    recent_ride_totals: {
+        distance: number,
+        achievement_count: number,
+        count: number,
+        elapsed_time: number,
+        elevation_gain: number,
+        moving_time: number
+    },
+    biggest_climb_elevation_gain: number,
+    ytd_ride_totals: string,
+    all_ride_totals: string,
+    ytd_run_totals: string
+}
+export async function load() {
+    try {
+        const key = STRAVA_KEY;
+        const response = await fetch('https://strava.com/api/v3/athletes/17975433/activities?access_token=' + key);
+        const json = await response.json();
+        return {  data: json };
+    } catch (error) {
+        console.log(error);
+    }
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,32 @@
+<script lang="ts">
+    import { onDestroy, onMount } from "svelte";
+    import { activitiesStore } from "../stores/store";
+    // import { load } from "./+page.server";
+
+    // let unsubscribe: () => void;
+    export let data;
+
+
+    fetchActivities();
+    function fetchActivities() {
+        // const json = load();
+        // console.log(json);
+        activitiesStore.subscribe(console.log);
+        console.log(data);
+        // unsubscribe = activitiesStore.subscribe(activities => {
+        //     console.log(activities)    
+        // })
+
+    }
+
+    // onMount(fetchActivities);
+
+    onDestroy(() => {
+    //   unsubscribe();
+    });
+
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <p>Total distance run: {data.all_run_totals.distance / 1000} km</p> -->
