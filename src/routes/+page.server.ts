4cb5e6c7321650a40fc334b6293f3f60385dc0e3
Luis Romero <luis.romero@atlascopco.com>
2024-03-14 14:58:49 +0100

    refactor(server): Remove unused interface

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,23 +1,3 @@
-interface Stats {
-    recent_run_totals: string,
-    all_run_totals: string,
-    recent_swim_totals: string,
-    biggest_ride_distance: number,
-    ytd_swim_totals: string,
-    all_swim_totals: string,
-    recent_ride_totals: {
-        distance: number,
-        achievement_count: number,
-        count: number,
-        elapsed_time: number,
-        elevation_gain: number,
-        moving_time: number
-    },
-    biggest_climb_elevation_gain: number,
-    ytd_ride_totals: string,
-    all_ride_totals: string,
-    ytd_run_totals: string
-}
 export async function load() {
     try {
         const headers = {
