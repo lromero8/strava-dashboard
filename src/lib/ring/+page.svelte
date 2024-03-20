b4d52620ced5bcc686da5c46bb269d06a73f189c
Luis Romero <luis.romero@atlascopco.com>
2024-03-20 15:55:39 +0100

    feat(client): Add icon inside activiy ring.

--- src/lib/ring/+page.svelte
+++ src/lib/ring/+page.svelte
@@ -1,28 +1,36 @@
 <script lang='ts'>
+    import Icon from '$lib/icons/+page.svelte';
+
     export let activityValue = '';
-    export let activityProperty = '';
+    export let activityIcon = '';
     export let percentageAchieved = 0;
     
     
     // 400px = 0%
     // 0px = 100%
     const strokeDashoffset = ((100 - percentageAchieved) / 100) * 400;
+    const ringStrokeColor = '#F9725C';
 
 </script>
 
 <div class="circle circle-big">
     <svg height="150" width="150" style="--stroke-dashoffset: {strokeDashoffset}">
-      <circle cx="80" cy="74" r="64" class="line-shadow" stroke="#000000" stroke-width="10" fill="none"></circle>
-      <circle cx="80" cy="74" r="64" class="line line-1" stroke="#F9725C" stroke-width="10" fill="none"></circle>
+        <circle cx="80" cy="74" r="64" class="line-shadow" stroke="#000000" stroke-width="7" fill="none"></circle>
+        <circle cx="80" cy="74" r="64" class="line line-1" stroke={ringStrokeColor} stroke-width="7" fill="none"></circle>
     </svg>
     
     <div class="info">
         <span class="info__top">{activityValue}</span>
-        <span class="info__bottom">{activityProperty}</span>
+        <!-- <span class="info__bottom">{activityIcon}</span> -->
+        <span class="info__icon">
+            <Icon name={activityIcon} size='28' color='#A5A7A9' />
+        </span>
     </div>
 </div>
 
 <style lang='scss'>
+    // Original design
+    // https://codepen.io/dianastanciu/pen/bGbwVNr
 
     svg {
         transform: rotate(-90deg);
@@ -65,17 +73,22 @@
         
         &__top {
             color: #000;
-            font-size: 1.4em;
+            font-size: 1.1rem;
         }
         
-        &__bottom {
-            color: #648192;
-            font-size: 0.8em;
+        // &__bottom {
+        //     color: #648192;
+        //     font-size: 0.8em;
+        //     display: block;
+        // }
+
+        &__icon {
             display: block;
+            margin: 10px 0 0 0;
         }
     }
     
     .line-shadow {
         stroke: rgba(0,0,0,.3);
     }
-    </style>
+</style>
