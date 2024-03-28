98011a639ff22a78e649ee4da6b66ba95d9a8faf
Luis Romero <luis.romero@atlascopco.com>
2024-03-28 13:18:22 +0100

    feat(client): Add swims overview.

--- src/lib/swims/+page.svelte
+++ src/lib/swims/+page.svelte
@@ -1,10 +1,16 @@
 <script lang='ts'>
     import type { Activity } from '$lib/activity';
+    import SwimsOverview from '$lib/swims/swims-overview/+page.svelte';
 
     export let swims: Activity[]|undefined;
 
 </script>
 
 {#if swims?.length}
-    <p>Last swim: {swims[0].distance / 1000} km</p>
+    <SwimsOverview swims={swims}/>
+
+    <!-- {#each runs as run}
+        <RunTile run={run} />
+    {/each} -->
+
 {/if}
---
+++ src/lib/swims/swims-overview/+page.svelte
@@ -0,0 +1,103 @@
+<script lang='ts'>
+    import {
+        getThisWeekActivities,
+        calculateTotalAvgPace,
+        calculateTotalBeersEarned,
+        calculateTotalCaloriesBurned,
+        calculateTotalDistance,
+        calculateTotalMovingTime,
+        formatTime
+    } from '$lib/shared/activities-helper';
+    import Icon from '$lib/icons/+page.svelte';
+    import type { Activity } from '$lib/activity';
+
+    export let swims: Activity[];
+
+    const thisWeekSwims = getThisWeekActivities(swims);
+    const distanceSwum = calculateTotalDistance(thisWeekSwims);
+    const movingTime = calculateTotalMovingTime(thisWeekSwims);
+    const formattedTime = formatTime(movingTime);
+    const avgPace = calculateTotalAvgPace(thisWeekSwims, movingTime);
+    const caloriesBurned = calculateTotalCaloriesBurned(movingTime);
+    const beersEarned = calculateTotalBeersEarned(caloriesBurned);
+    console.log(swims);
+
+</script>
+<div class='fifi-card-overview'>
+    <span class='fifi-card-overview-title'>This week's overview</span>
+    <div class="fifi-card-overview-body">
+
+        <div class="fifi-card-col">
+            <p class='fifi-body-item'>
+                <Icon name='Swim' size='22' color='white' />
+                <span>Distance swum: {distanceSwum.toPrecision(3)} kms</span>
+            </p>
+            <p class='fifi-body-item'>
+                <Icon name='TimerOutline' size='22' color='white' />
+                <span>Moving time: {formattedTime}</span>
+            </p>
+        </div>
+
+        <div class="fifi-card-col">
+            <p class='fifi-body-item'>
+                <Icon name='Speedometer' size='22' color='white' />
+                <span>Average pace: {avgPace.toPrecision(3)} min/km</span>
+            </p>
+            <p class='fifi-body-item'>
+                <Icon name='GlassMugVariant' size='22' color='white' />
+                <span>Kölschs earned: {beersEarned.toPrecision(2)}</span>
+            </p>
+        </div>
+    </div>
+</div>
+
+<style lang='scss'>
+    $card-bg-color: #0090E3;
+
+    div.fifi-card-overview {
+        display: flex;
+        flex-direction: column;
+        border: 1px solid #e4e6ed;
+        border-radius: 5px;
+        box-shadow: 0 5px 5px 0 rgba(69, 88, 127, 0.1);
+        margin: 30px auto;
+        width: 38rem; 
+        padding: 1em;
+        @media screen and (max-width: 750px) {
+            width: 20rem;
+        }
+
+        background-color: $card-bg-color;
+        color: #fff;
+        border: 2px solid transparent;
+        border-radius: 4px;
+    }
+    
+    span.fifi-card-overview-title {
+        font-size: 1.1rem;
+    }
+    
+    div.fifi-card-overview-body {
+        display: flex;
+        justify-content: space-around;
+        @media screen and (max-width: 750px) {
+            flex-direction: column;
+        }
+
+        margin-top: 15px;;
+    }
+
+    div.fifi-card-col {
+        display: flex;
+        flex-direction: column;
+    }
+    
+    p.fifi-body-item {
+        display: flex;
+        align-items: center;
+
+        span {
+            margin-left: 5px;
+        }
+    }
+</style>
