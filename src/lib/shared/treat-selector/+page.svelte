7e94f9ff97405a3b4fadb1dd558f2630e5941276
Luis Romero <luis.romero@atlascopco.com>
2024-04-05 12:36:49 +0200

    feat(client): Support color as inputs.

--- src/lib/shared/treat-selector/+page.svelte
+++ src/lib/shared/treat-selector/+page.svelte
@@ -9,6 +9,9 @@
     }
 
     export let caloriesBurned: number;
+    export let iconColor: string;
+    export let selectedTextColor: string;
+    export let selectedValueTextColor: string;
 
     const treats: Treat[] = [
         {
@@ -69,80 +72,106 @@
     });
 </script>
 
-
-<div class="fifi-dropdown" class:open={isOpen}>
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <!-- svelte-ignore a11y-missing-attribute -->
-    <!-- svelte-ignore a11y-no-static-element-interactions -->
-    <a on:click={toggleDropdown}>
-        <Icon name={treats[selectedTreatIndex].icon} size='22' color='white' />
-        <span>{treats[selectedTreatIndex].name}</span>
-    </a>
-    <div class="fifi-dropdown-content">
-        {#each treats as treat, index}
-            <!-- svelte-ignore a11y-click-events-have-key-events -->
-            <!-- svelte-ignore a11y-no-static-element-interactions -->
-            <!-- svelte-ignore a11y-missing-attribute -->
-            <span on:click={() => calculate(index)}>
-                <Icon name={treat.icon} size='22' color='#0090E3' />
-                <a>{treat.name}</a>
-            </span>
-        {/each}
+<span class="fifi-dropdown-container">
+    <div class="fifi-dropdown" class:open={isOpen}>
+        <!-- svelte-ignore a11y-click-events-have-key-events -->
+        <!-- svelte-ignore a11y-missing-attribute -->
+        <!-- svelte-ignore a11y-no-static-element-interactions -->
+        <a on:click={toggleDropdown} style="--selected-option-text-color: {selectedTextColor}">
+            <Icon name={treats[selectedTreatIndex].icon} size='22' color={iconColor} />
+            <span>{treats[selectedTreatIndex].name}</span>
+        </a>
+        <div class="fifi-dropdown-content">
+            {#each treats as treat, index}
+                <!-- svelte-ignore a11y-click-events-have-key-events -->
+                <!-- svelte-ignore a11y-no-static-element-interactions -->
+                <!-- svelte-ignore a11y-missing-attribute -->
+                <span on:click={() => calculate(index)}>
+                    <Icon name={treat.icon} size='22' color='#0090E3' />
+                    <a>{treat.name}</a>
+                </span>
+            {/each}
+        </div>
     </div>
-</div>
-<span>earned: {treatsEarned}</span>
+    <span class="fifi-earned-text" style="--earned-text-color: {selectedTextColor}">earned: </span>
+</span>
+
+<span class="fifi-earned-value-text" style="--earned-value-text-color: {selectedValueTextColor}">{treatsEarned}</span>
 
 <style lang="scss">
-  .fifi-dropdown {
-    position: relative;
-  }
 
-  .fifi-dropdown a {
-    display: flex;
-    align-items: center;
-    color: white;
-    border: none;
-    cursor: pointer;
+    $selected-option-text-color: var(--selected-option-text-color);
+    $dropdown-content-bg-color: white;
+    $dropdown-option-text-color: black;
+    $dropdown-option-hover-color: #f1f1f1;
+    $earned-text-color: var(--earned-text-color);
+    $earned-value-text-color: var(--earned-value-text-color);
 
-    text-decoration: underline;
-    text-decoration-color: white;
-    
-    span {
-        margin-left: 5px;
-        margin-right: 5px;
+    .fifi-dropdown-container {
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
     }
-  }
 
-  .fifi-dropdown .fifi-dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: white;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-  }
-
-  .fifi-dropdown .fifi-dropdown-content span {
-    display: flex;
-    align-items: center;
-    a {
-        color: black;
-        padding: 12px 16px;
-        text-decoration: none;
-        font-size: 0.9rem;
-        text-wrap: nowrap;
+    .fifi-dropdown {
+        position: relative;
     }
-    padding: 5px 7px;
-  }
 
-  .fifi-dropdown .fifi-dropdown-content span:hover {
-    background-color: #f1f1f1;
-    cursor: pointer;
-  }
+    .fifi-dropdown a {
+        display: flex;
+        align-items: center;
+        color: $selected-option-text-color;
+        border: none;
+        cursor: pointer;
 
-  .fifi-dropdown.open .fifi-dropdown-content {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-  }
+        text-decoration: underline;
+        text-decoration-color: $selected-option-text-color;
+        
+        span {
+            margin-left: 10px;
+            margin-right: 5px;
+        }
+    }
+
+    .fifi-dropdown .fifi-dropdown-content {
+        display: none;
+        position: absolute;
+        background-color: $dropdown-content-bg-color;
+        min-width: 160px;
+        box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+        z-index: 1;
+    }
+
+    .fifi-dropdown .fifi-dropdown-content span {
+        display: flex;
+        align-items: center;
+        a {
+            color: $dropdown-option-text-color;
+            padding: 12px 16px;
+            text-decoration: none;
+            font-size: 0.9rem;
+            text-wrap: nowrap;
+        }
+        padding: 5px 7px;
+    }
+
+    .fifi-dropdown .fifi-dropdown-content span:hover {
+        background-color: $dropdown-option-hover-color;
+        cursor: pointer;
+    }
+
+    .fifi-dropdown.open .fifi-dropdown-content {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+
+    .fifi-earned-text {
+        color: $earned-text-color;
+        margin-right: 10px;
+    }
+
+    .fifi-earned-value-text {
+        color: $earned-value-text-color;
+    }
 </style>
